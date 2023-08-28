a0b2ded839405a42d87ce8c1bfc1028807ab2df3
subhadeep <srsubhadeepghosh456@gmail.com>
2023-08-28 10:40:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const body = document.body;
+body.style.backgroundColor = "red"
+console.log(body);
+
+const box2 = document.getElementById('box-2');
+console.log(box2)
+
+const divs = document.getElementsByTagName('div');
+console.log(divs);
+
+const p = document.getElementById('paragraph');
+console.log(p)
+p.innerHTML = "<h1>This is subhadeep </h1>";
+p.style.color = "white"
